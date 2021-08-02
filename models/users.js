f6f622aa8105417abd9e3b17f715f958a6f96466
Kausar Akther <kausar.akther@gmail.com>
2021-08-02 14:05:56 -0400

    Added users schema

--- models/users.js
+++ models/users.js
@@ -2,8 +2,10 @@ const mongoose = require ('mongoose');
 const Schema = mongoose.Schema
 
 const userSchema = Schema({
-    username: { type: String, unique: true, require: true},
-    password: String
+    username: { type: String, unique: true, require: true, minLength: 5, maxLength: 20},
+    email: {type: String, unique: true, require: true},
+    password: {type: String, require: true, minLength: 8},
+    userTypeId: {type: Number, require: true, default: 2}
 })
 
 const User = mongoose.model('User', userSchema)
